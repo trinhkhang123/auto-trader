e4210f0882cf7da55afcf88f282e52fd20d0bb1d
trinhkhang123 <khangmau12k@gmail.com>
2025-08-15 11:30:51 +0700

    update

--- trading_api.py
+++ trading_api.py
@@ -345,4 +345,4 @@ def cancel_order():
 if __name__ == '__main__':
     print("Flask starting...")  # thêm dòng này
     port = os.getenv('PORT', 5001)
-    app.run(host='0.0.0.0', port=port, debug=True)
+    app.run(host='0.0.0.0', port=port, debug=False)
